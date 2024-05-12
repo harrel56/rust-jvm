cb3bcc134362cc4f3adb2485a20f0c539747c621
harrel <marcin.slowik95@gmail.com>
2024-05-12 17:12:19 +0200

    change java file

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
+import java.io.Serializable;
 
-
-public class Main {
+public class Main implements Serializable {
     public static void main(String[] args) {
         System.out.println("Hello java!");
     }
